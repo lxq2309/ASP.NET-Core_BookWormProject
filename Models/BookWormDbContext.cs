889ce8d06050c63deb0d02a1b7fb035286368609
lxq <lxq.2309@gmail.com>
2023-05-22 11:17:26 +0700

    awesome deploy

--- Models/BookWormDbContext.cs
+++ Models/BookWormDbContext.cs
@@ -33,7 +33,7 @@ public partial class BookWormDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=ADMIN-PC;Initial Catalog=BookWormDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False; MultipleActiveResultSets=True");
+        => optionsBuilder.UseSqlServer("workstation id=BookWormDB.mssql.somee.com;packet size=4096;user id=lxq2309_SQLLogin_1;pwd=sr71vkwq3f;data source=BookWormDB.mssql.somee.com;persist security info=False;Initial Catalog=BookWormDB;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False; MultipleActiveResultSets=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
